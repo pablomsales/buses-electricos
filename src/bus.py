e90c31ae734c70ac51bcf12cee0b7796d5550147
razmikuniovi <uo288030@uniovi.es>
2024-07-17 16:52:47 +0200

    adición de las características del bus

--- src/bus.py
+++ src/bus.py
@@ -1,7 +1,10 @@
 class Bus:
 
-    def __init__(self, mass):
-        self._mass = 0
+    def __init__(self, mass, drag_coefficient, frontal_area, rolling_resistance_coefficient):
+        self._mass = mass
+        self._drag_coefficient = drag_coefficient
+        self._frontal_area = frontal_area
+        self._rolling_resistance_coefficient = rolling_resistance_coefficient
 
     @property
     def mass(self):
@@ -10,4 +13,31 @@ class Bus:
     @mass.setter
     def mass(self, value):
         if value > 0:
-            self._mass += value
+            self._mass = value
+
+    @property
+    def drag_coefficient(self):
+        return self._drag_coefficient
+
+    @drag_coefficient.setter
+    def drag_coefficient(self, value):
+        if 0 < value < 1:  # typical values for drag coefficient
+            self._drag_coefficient = value
+
+    @property
+    def frontal_area(self):
+        return self._frontal_area
+
+    @frontal_area.setter
+    def frontal_area(self, value):
+        if value > 0:
+            self._frontal_area = value
+
+    @property
+    def rolling_resistance_coefficient(self):
+        return self._rolling_resistance_coefficient
+
+    @rolling_resistance_coefficient.setter
+    def rolling_resistance_coefficient(self, value):
+        if value > 0:
+            self._rolling_resistance_coefficient = value

b73c5db9e56c210660d235b605913391aac9ddd7
razmikuniovi <uo288030@uniovi.es>
2024-07-22 19:21:49 +0200

    main

--- src/__main__.py
+++ src/__main__.py
@@ -3,16 +3,28 @@ from time import time
 
 from bus import Bus
 from route import Route
+from engine import Engine
 
 
 def main():
     start_time = time()
-    data = os.path.join("data", "linea_D1_parcial.csv")
+    data = os.path.join("data", "linea_d2.csv")
+
+    # Crear una instancia de Engine
+    engine_instance = Engine(
+        type='electric',  # Puede ser 'combustion' o 'electric'
+        max_torque=400,  # Nm
+        max_power=200,  # kW
+        efficiency=0.9  # 0 a 1
+    )
+
+    # Crear una instancia de Bus con el motor
     bus_instance = Bus(
         mass=12000,
         drag_coefficient=0.65,
         frontal_area=8.0,
         rolling_resistance_coefficient=0.01,
+        engine=engine_instance  # Pasar la instancia de Engine
     )
 
     try:
@@ -21,7 +33,7 @@ def main():
         for section in route_instance.sections:
             print(section)
 
-        route_instance.plot_map(output_file="linea_D1_parcial.html")
+        route_instance.plot_map(output_file="linea_D2.html")
         print(f"Tiempo ejecucion: {time() - start_time}")
 
     except ValueError as e:

876950fab42189491d13594d98df4e827522b555
pablosales-uniovi <uo289916@uniovi.es>
2024-07-24 14:32:42 +0200

    Añadida clase Fuel

---
+++ src/fuel.py
@@ -0,0 +1,28 @@
+class Fuel:
+    # Class-level dictionary with predefined fuels and their PCI values in J/L
+    _fuel_data = {
+        "gasoline": 3.1536e7,  # J/L
+        "diesel": 3.58e7,  # J/L
+        "propane": 2.5e7,  # J/L
+        "natural_gas": 3.6e7,  # J/L
+        "E85": 2.4e7,  # J/L
+        "E100": 2.68e7,  # J/L
+    }
+
+    def __init__(self, fuel_type, pci=None):
+        self._fuel_type = fuel_type
+        if fuel_type in Fuel._fuel_data:
+            self._pci = Fuel._fuel_data[fuel_type]
+        else:
+            if pci:
+                self._pci = pci
+            else:
+                raise ValueError("You must provide the PCI for this fuel type")
+
+    @property
+    def fuel_type(self):
+        return self._fuel_type
+
+    @property
+    def pci(self):
+        return self._pci

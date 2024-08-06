980173ca3d6cca573cd1efd088ccc0467cc6d04f
pablosales-uniovi <uo289916@uniovi.es>
2024-08-06 12:45:40 +0200

    a

--- src/core/section/estimated_section.py
+++ src/core/section/estimated_section.py
@@ -1,6 +1,6 @@
-from src.core.section import BaseSection
+from core.section import base_section
 
 
-class EstimatedSection(BaseSection):
+class EstimatedSection(base_section):
     def __init__(self, coordinates, bus, emissions):
         super().__init__(coordinates, bus, emissions)

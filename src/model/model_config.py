f6eae9140b2dd6a798b1dcee117adde7a10177f5
pablosales-uniovi <uo289916@uniovi.es>
2024-08-29 19:46:07 +0200

    Modificada clase Config para que sea ModelConfig. Permitido paso de parametros a metodos de create_bus() y avance del esquema general en clase Fitness

--- src/model/model_config.py
+++ src/model/model_config.py
@@ -6,7 +6,7 @@ from core.bus.fuel import Fuel
 from core.route.emissions import Emissions
 
 
-class Config:
+class ModelConfig:
     def __init__(self, electric, euro_standard="EURO_6"):
         """
         Class to create the configuration of the bus and emissions.
@@ -21,7 +21,7 @@ class Config:
         self.electric = electric
         self.euro_standard = euro_standard
 
-    def create_bus(self):
+    def create_bus(self, initial_capacity_kWh, max_power, bus_mass):
         """
         Create a bus instance with the engine and fuel selected.
 
@@ -32,15 +32,19 @@ class Config:
         """
         if self.electric:
             # Create an electric bus instance
-            bus_instance = self._create_electric_bus()
+            bus_instance = self._create_electric_bus(
+                initial_capacity_kWh, max_power, bus_mass
+            )
 
         else:
             # Create a fuel bus instance
-            bus_instance = self._create_fuel_bus()
+            bus_instance = self._create_fuel_bus(max_power, bus_mass)
 
         return bus_instance
 
-    def _create_electric_bus(self):
+    def _create_electric_bus(
+        self, initial_capacity_kWh=294, max_power=240000, bus_mass=20000
+    ):
         """
         Create an electric bus instance with the battery and electrical engine selected.
 
@@ -51,8 +55,8 @@ class Config:
         """
         # Create a battery instance
         battery_instance = Battery(
-            initial_capacity_kWh=294,
-            voltage_v=400,
+            initial_capacity_kWh=initial_capacity_kWh,
+            voltage_v=400,  # parametro estatico para datathon
             max_cycles=3000,  # parametro estatico para datathon
             initial_soc_percent=100,  # parametro estatico para datathon
             min_state_of_health=80,  # parametro estatico para datathon
@@ -60,14 +64,14 @@ class Config:
 
         # Create an electrical engine instance
         engine_instance = ElectricalEngine(
-            max_power=240000,
+            max_power=max_power,
             efficiency=0.92,  # parametro estatico para datathon (0 a 1)
             battery=battery_instance,
         )
 
         # Create a bus instance
         bus_instance = Bus(
-            mass=20000,
+            mass=bus_mass,
             drag_coefficient=0.8,  # parametro estatico para datathon
             frontal_area=9.0,  # parametro estatico para datathon
             rolling_resistance_coefficient=0.01,  # parametro estatico para datathon
@@ -76,7 +80,7 @@ class Config:
 
         return bus_instance
 
-    def _create_fuel_bus(self):
+    def _create_fuel_bus(self, max_power=240000, bus_mass=20000):
         """
         Create a fuel bus instance with the fuel engine selected.
 
@@ -91,13 +95,13 @@ class Config:
         # Create a fuel engine instance
         engine_instance = FuelEngine(
             fuel=fuel_instance,
-            max_power=200000,
+            max_power=max_power,
             efficiency=0.35,  # 0 a 1
         )
 
         # Create a bus instance
         bus_instance = Bus(
-            mass=20000,
+            mass=bus_mass,
             drag_coefficient=0.8,
             frontal_area=9.0,
             rolling_resistance_coefficient=0.01,
--- src/optimization/fitness.py
+++ src/optimization/fitness.py
@@ -1,3 +1,32 @@
+from model.model_config import ModelConfig
+from src.model.core.model import Model
+
+
 class Fitness:
 
-    def __init__(self): ...
+    def __init__(self):
+        self.model_config = ModelConfig(electric=True)
+
+    def evaluate(self, parameters: dict):
+
+        # desempaquetar diccionario de parameters
+
+        # Establecer parametros en ModelConfig
+        bus = self.model_config.create_bus(
+            initial_capacity_kWh,
+            engine_max_power,
+            bus_mass,  # antes que nada hay que sumar bus_mass y battery_mass !!!
+            time_between_charges,
+        )
+        emissions = self.model_config.create_bus()
+
+        # Inicializar Model() con ModelConfig()
+        model_name = ...
+        data = ...
+        model = Model(
+            name=model_name,
+            filepath=data,
+            bus=bus,
+            emissions=emissions,
+            mode="simulation",
+        )

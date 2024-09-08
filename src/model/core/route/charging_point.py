89fc059cd7908ecf8cec23055cc237528d8c5065
razmikuniovi <uo288030@uniovi.es>
2024-09-08 23:25:23 +0200

    Añadidos docstrings ChargingPoint

--- src/model/core/route/charging_point.py
+++ src/model/core/route/charging_point.py
@@ -1,52 +1,51 @@
 class ChargingPoint:
     """
-    Class to represent a charging point.
+    Clase para representar un punto de carga.
     """
     def __init__(self, id, bus, coordinates, max_power, distance, time):
         """
-        Initialize a ChargingPoint instance.
+        Inicializa una instancia de ChargingPoint.
 
-        Parameters
+        Parámetros
         ----------
         id : int
-            Unique identifier for the charging point.
+            Identificador único para el punto de carga.
         bus : Bus
-            The bus instance that will be charged.
+            La instancia del autobús que será cargado.
         coordinates : tuple
-            Represents the coordinates of the charging point.
+            Representa las coordenadas del punto de carga.
         max_power : float
-            Maximum power output of the charging point in kW.
+            Potencia máxima de salida del punto de carga en kW.
         distance : float
-            Distance from the last point of the route to the charging point in kilometres.
+            Distancia desde el último punto de la ruta hasta el punto de carga en kilómetros.
         time : float
-            Time from the last point of the route to the charging point in minutes.
+            Tiempo desde el último punto de la ruta hasta el punto de carga en minutos.
         """
         self.id = id
         self._bus = bus
         self.coordinates = coordinates
         self.max_power = max_power
-        self.distance = distance # kilometres
-        self.time = time # minutes
+        self.distance = distance # kilómetros
+        self.time = time # minutos
 
     def cost(self):
         """
-        Calculate the cost (electric consumption) of driving to the charging point.
+        Calcula el costo (consumo eléctrico) de conducir hasta el punto de carga.
 
         Returns
         -------
         tuple
-            Tuple containing the electric consumption in Wh and Ah.
+            Tupla que contiene el consumo eléctrico en Wh y Ah.
         """
         power = self._bus.engine.max_power
-        hours = self.time / 60 # convert minutes to hours
+        hours = self.time / 60 # convertir minutos a horas
         voltage = self._bus.engine.battery.voltage_v
 
-        # Compute consumption in Wh and Ah
+        # Calcular el consumo en Wh y Ah
         watts_hour = power * hours
         ampers_hour = watts_hour / voltage
 
         return watts_hour, ampers_hour
 
-
     def __str__(self):
-        return f"ChargingPoint {self.id} at {self.location} with max power {self.max_power} kW, at {self.time} minutes and {self.distance} km away"
+        return f"Punto de carga {self.id} en {self.coordinates} con potencia máxima de {self.max_power} kW, a {self.time} minutos y {self.distance} km de distancia"
